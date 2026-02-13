78321b8c844986dd6685f793abee4a012e7eff71
Saveri23 <gavvalasaveri89@gmail.com>
2026-02-13 11:01:56 +0530

    Initial commit2

--- src/main/java/com/example/itemapi/service/ItemService.java
+++ src/main/java/com/example/itemapi/service/ItemService.java
@@ -1,3 +1,12 @@
+package com.example.itemapi.service;
+
+import com.example.itemapi.model.Item;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ItemService {
 
